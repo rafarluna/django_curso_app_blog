41b2d95a7551c7147fb34239597dc94123f8805d
Martin Quinta <dark.kase@gmail.com>
2019-08-31 13:48:49 -0500

    extending api view class

--- eventos/apis/serializers.py
+++ eventos/apis/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from ..models import *
+from rest_framework.exceptions  import NotAcceptable
+
 
 class EventsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'name', 'description', 'datetime', 'place']
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = ['id', 'event', 'user', 'type']
+
+    def validate(self, data):
+        if Attendance.objects.filter(event=data['event'], user=data['user']).exists():
+            raise serializers.ValidationError("usuario ya registrado")
+        return data
--- eventos/apis/views.py
+++ eventos/apis/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 from ..models import *
-from .serializers import EventsSerializer
+from .serializers import *
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class EventsAPIView(generics.ListCreateAPIView):
@@ -14,3 +16,26 @@ class EventAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = EventsSerializer
     permission_classes = [IsAuthenticated]
+
+
+class EventAttendanceAPIView(generics.ListCreateAPIView):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    permission_classes = [IsAuthenticated]
+
+    @property
+    def pk(self):
+        return self.kwargs.get('pk')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(event=self.pk)
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['event'] = self.pk
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
